daa24f717f639849fd90fefd9c3cb331cb613948
RedCat771 <savelyev7979@gmail.com>
2021-02-02 18:25:09 +0200

    add reviews section

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,63 @@
-// слайдер
-const swiper = new Swiper(".swiper-container", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  // слайдер шапки
+  const swiper = new Swiper(".header__container", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    lazy: true,
+  });
+
+  // слайдер отзывов
+  const swiper2 = new Swiper(".reviews__container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    lazy: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+
+  // рейтинг
+  const ratings = document.querySelectorAll(".rating");
+
+  if (ratings.length > 0) {
+    initRatings();
+  }
+
+  // Функция запуска рейтинга
+  function initRatings() {
+    let ratingActive, ratingValue;
+    for (let index = 0; index < ratings.length; index++) {
+      const rating = ratings[index];
+      initRating(rating);
+    }
+
+    // инициилизируем рейтинг
+    function initRating(rating) {
+      initRatingVars(rating);
+      setRatingActiveWith();
+    }
+
+    // инициализация переменных
+    function initRatingVars(rating) {
+      ratingActive = rating.querySelector(".rating__active");
+      ratingValue = rating.querySelector(".rating__value");
+    }
+
+    // меняем ширину активных звезд
+    function setRatingActiveWith(index = ratingValue.innerHTML) {
+      const ratingActiveWidth = index / 0.05;
+      ratingActive.style.width = `${ratingActiveWidth}%`;
+    }
+  }
 });
